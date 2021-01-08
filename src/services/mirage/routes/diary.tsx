6faad95a12a4678326a238db8758bd9fd74c9f4e
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-08 12:15:19 -0800

    adding entries

--- src/services/mirage/routes/diary.tsx
+++ src/services/mirage/routes/diary.tsx
@@ -46,4 +46,66 @@ export const updateDiary = (schema: any, req: Request): Diary | Response => {
     }catch (error) {
         return handleErrors(error, "Failed to update Diary")
     }
+};
+export const getDiaries = (schema: any,req: Request ): Diary[] | Response => {
+    try{
+        const user = schema.users.find(req.params.id);
+        return user.diary as Diary[]; 
+    }catch (error){
+        return handleErrors(error, 'Could not get user diaries');
+    }
+};
+///working with entries
+
+export const addEntry = (
+    schema: any,
+    req: Request
+): {diary: Diary; entry: Entry} | Response => {
+    try{
+        const diary = schema.diaries.find(req.params.id);
+        const { title,content } = JSON.parse(req.requestBody) as Partial<Entry>;
+        const now = dayjs().format();
+        const entry = diary.createEntry({
+            title,
+            content,
+            createdAt: now,
+            updatedAt: now,
+        });
+        diary.update({
+            ...diary.attrs,
+            updatedAt: now,
+        });
+        return{
+            diary: diary.attrs,
+            entry: entry.attrs,
+        };
+    }catch(error){
+        return handleErrors(error, 'Failed to save entry');
+    }
+};
+
+export const getEntries = (
+    schema: any,
+    req: Request
+): {entries: Entry[]} | Response => {
+    try{
+        const diary = schema.diaries.find(req.params.id);
+        return diary.entry;
+    }catch(error){
+        return handleErrors(error,'Failed to get Diary entreis');
+    }
+};
+export const updateEntry = (schema: any, req: Request): Entry | Response => {
+    try{
+        const entry = schema.entries.find(req.params.id);
+        const data = JSON.parse(req.requestBody) as Partial<Entry>;
+        const now = dayjs().format()
+        entry.update({
+          ...data,
+          updatedAt: now,
+        });
+        return entry.attrs as Entry;
+    }catch(error){
+        return handleErrors(error, "Failed to update entry");
+    }
 }
