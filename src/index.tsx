4f9d18abd435d38324214994bb2e3fd8ec85a52a
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-08 14:00:46 -0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App/App';
 import { setupServer } from './services/mirage/server';
+import store from './store/store';
+import { Provider } from 'react-redux';
 
 if(process.env.NODE_ENV === 'development') {
   setupServer();
 }
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
