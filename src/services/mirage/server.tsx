4744c949697312c941a62cb9adbfd8b78f798ff6
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-08 10:54:54 -0800

    setup server with miragejs

---
+++ src/services/mirage/server.tsx
@@ -0,0 +1,46 @@
+import { Server, Model, Factory, belongsTo, hasMany, Response } from 'miragejs';
+
+///handleErrors response, if any problem occured
+export const handleErrors = (error:any, message='An error occured'  ) => {
+    return new Response(400, undefined, {
+        data: {
+            message,
+            isError: true,
+        },
+    });
+};
+
+export const setupServer = (env?: string): Server => {
+    return new Server({
+        environment: env ?? 'development',
+        
+        //creating models for dynamics data
+        models: {
+            entry: Model.extend({
+                diary: belongsTo(),
+            }),
+            diary: Model.extend({
+                entry: hasMany(),
+                user: belongsTo(),
+            }),
+            user: Model.extend({
+                diary: hasMany()
+            }),
+        },
+        
+        ///Factories are the perfect place to encode the constraints of your seeding data
+        factories: {
+            user: Factory.extend({
+               username: 'test',
+               password: "password",
+               email: "Shazain@gmail.com",
+            }),
+        },
+        seeds: (server): any => {
+            server.create('user');
+        },
+        routes(): void{
+            this.urlPrefix = 'https://diaries.app';
+        }
+    })
+}
